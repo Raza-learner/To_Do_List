463437328b61dedbeed1b3a094dd5ef358b90f1a
Shaikh Mohammad Raza <mohammadrazashaikh59@gmail.com>
2025-04-05 11:08:58 +0530

    Working

--- to_do.py
+++ to_do.py
@@ -1,24 +1,31 @@
+task=[]
 def main():
-    task=[]
-
+    print("\nWelcome to TODO TUI")
+    print("---------------------")
+    
     while True:
-        print("\n====To do List===")
+        print("---Select The Choice--")
         print("1.Add Task")
         print("2.Show Task")
-        print("3.Update List")
-        print("4.Mark as Done")
-        print("5.exit")
+        print("3.Delete Task")
+        print("4.Exit")
+
+        choice = int(input("Enter the Choice: "))
         
-    choice=int(input("Enter the choice:"))
-    
-    if choice=="1":
-        add = int(input("Add:"))
+        if choice==1:
+            Add=input("Enter the task: ").split()       
+            task.append(Add)
+            print("Task added!")
+        elif choice==2:
+            for i in range(len(task)):
+                print(i+1,task[i])
+        elif choice==3:
+            index_del=int(input("Enter the task number to delete: "))
+            del task[index_del+1]
+        elif choice == 4:
+                break
+        else:
+            print("Invalid input")
 
-        for i in range(add):
-            task = input("Enter task: ")
-            task.append(task)
-            print("Task Added.")
-
-
-
-
+if __name__=="__main__":
+    main()
