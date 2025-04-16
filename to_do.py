1acb83df133b78f3b8a265e298967c72b88a55d0
Shaikh Mohammad Raza <mohammadrazashaikh59@gmail.com>
2025-04-16 19:56:29 +0530

    changes

--- to_do.py
+++ to_do.py
@@ -1,24 +1,32 @@
+import os
 tasks=[]
-
+def print_list():
+    for idx, i in enumerate(tasks[0:5]):
+            print(f"{idx + 1}. {i}")
+    if len(tasks)>5:
+            print("--more--")
+    
+        
 def main():
     
+    
     while True:
-        for idx, i in enumerate(tasks[0:6]):
-            print(f"{idx + 1}. {i}")
-
-        
-
         print("[1]Add Task [2]Show All Task [3]Delete Task [4]Edit Task [5]Exit")
         choice = int(input("Enter Your Choice: "))
-
-        if choice == 1:
+       
+               
+        if choice == 1: 
+            os.system("cls")
             t = input("Enter the task/s:")
             tasks.append(t)
+            os.system("cls")
+            print_list()
 
         elif choice == 2:
+            os.system("cls")
             for idx, i in enumerate(tasks):
                 print(f"{idx + 1}. {i}")
-
+            
         elif choice == 3:
             index_del = int(input("Enter the task number to delete:"))
             del tasks[index_del - 1]
